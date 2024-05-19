6e842801320574b3010386c2951197721cdd3824
aniket2727 <aniketkadam5950@gmail.com>
2024-05-19 13:06:38 +0530

    add new file code

---
+++ Code1.js
@@ -0,0 +1,4 @@
+
+
+
+console.log("this is code 1")
--- Testter.js
+++ Testter.js
@@ -1,9 +1,9 @@
 
 
-<<<<<<< HEAD
-console.log("aniket kadam")
+
+
 console.log("this is sakshi kadam")
 console.log("this is om kadam")
-=======
+
 console.log("aniket  kadam")
->>>>>>> b6e6cf7c2eb0a5cbca4a34c2824f3330de0e5417
+
